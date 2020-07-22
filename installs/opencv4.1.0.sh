636f495b2a0bd728fcbc94e3bc5bde94074996b4
Torben Daniel Erz <3204695+torben-erz@users.noreply.github.com>
2020-07-22 13:52:06 +0200

    Update opencv4.1.0.sh

--- installs/opencv4.1.0.sh
+++ installs/opencv4.1.0.sh
@@ -1,1 +1,6 @@
-
+sudo apt-get install build-essential cmake pkg-config -y
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt-get install libxvidcore-dev libx264-dev -y
+sudo apt-get install libgtk2.0-dev libgtk-3-dev -y
+sudo apt-get install libatlas-base-dev gfortran -y
