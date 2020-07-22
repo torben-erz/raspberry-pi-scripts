32b4901e99f2dc60f11b65d57b9cb98d65216195
Torben Daniel Erz <3204695+torben-erz@users.noreply.github.com>
2020-07-22 19:13:38 +0200

    Create install.sh

---
+++ Drivers/1.54inch-lcd/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/torben-erz/raspberry-pi-scripts/master/drivers/1.54inch-lcd/LCD-show-190115.tar.gz -O LCD-show-190115.tar.gz
+tar xvf LCD-show-190115.tar.gz
+rm LCD-show-190115.tar.gz
+cd LCD-show
+#sudo ./LCD154-show
