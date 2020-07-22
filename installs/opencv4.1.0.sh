8838674c3a54fc29003e84b62a6d2946ff208c92
Torben Daniel Erz <3204695+torben-erz@users.noreply.github.com>
2020-07-22 14:54:22 +0200

    Update opencv4.1.0.sh

--- installs/opencv4.1.0.sh
+++ installs/opencv4.1.0.sh
@@ -1,22 +1,3 @@
-#
-create_clean_directory(){
-    dir_name=$1
-    if [ -d "$dir_name" ]; then
-        echo "Removing $dir_name"
-        rm -rf "$dir_name"
-    elif [ -f "$dir_name" ]; then
-        echo "File with this name already exists, not a directory."
-        exit
-    fi
-    if mkdir "$dir_name"; then
-        echo "Clean directory created: $dir_name"
-        return 0
-    else
-        echo "Creating directory failed: $dir_name"
-        return 1
-    fi 
-}
-
 # System update
 sudo apt-get update
 sudo apt-get upgrade
