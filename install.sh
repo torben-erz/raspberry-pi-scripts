a8301bc640190ca06f440a1076c08e53a82d76e4
Torben Daniel Erz <torben.erz@gmail.com>
2020-07-22 20:19:03 +0200

    no message

---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo ""
+echo "================================"
+echo "Welcome to Toverland-Pi Installation Script for Raspbian"
+echo "Maintained by Torben Daniel Erz (torben.erz@gmail.com)"
+echo ""
+
+
+# Step 1: Preparing
+echo ""
+echo "================================"
+echo "Preparing system for installation ..."
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+echo ""
+echo "Complete"
+echo ""
+
+
+# Step 2: Download scripts
+echo ""
+echo "================================"
+echo "Downloading scripts ..."
+wget https://raw.githubusercontent.com/torben-erz/raspberry-pi-scripts/master/python3.sh
+wget https://raw.githubusercontent.com/torben-erz/raspberry-pi-scripts/master/gpio.sh
+
+
+# Step 3: Execute installation script python3
+echo ""
+echo "================================"
+echo "Execute Installation of Python3 ..."
+sh ./python3.sh
+
+
+# Step 4: Execute installation script GPIO
+echo ""
+echo "================================"
+echo "Execute Installation of GPIO ..."
+sh ./gpio.sh
+
+
+# Step 5: Delete sh-scripts
+rm -rf python3.sh
+rm -rf gpio.sh
+
+
+# Step 6: Install Python3 IDLE
+sudo apt-get -y install idle3
