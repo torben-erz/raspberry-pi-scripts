3d2f8c81bbd68668c5124cd8d3e1ba25c4b14113
Torben Daniel Erz <3204695+torben-erz@users.noreply.github.com>
2020-07-22 15:18:10 +0200

    Update opencv4.0.1.sh

--- installs/opencv4.0.1.sh
+++ installs/opencv4.0.1.sh
@@ -12,5 +12,10 @@ sudo apt autoremove
 # Install tools and libraries for OpenCV
 sudo apt-get -y install libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev libqtgui4 libqt4-test
 
+# Install Paython3, pip3 and numpy
+wget https://raw.githubusercontent.com/torben-erz/raspberry-pi-scripts/master/installs/python3.sh
+sh python3.sh
+rm python3.sh
+
 # Install OpenCV
 pip3 install opencv-python==$version
