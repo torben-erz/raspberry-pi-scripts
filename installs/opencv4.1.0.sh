7d334ca3dc6a7fb2493feade45c508bd78a1052f
Torben Daniel Erz <3204695+torben-erz@users.noreply.github.com>
2020-07-22 15:12:21 +0200

    Update opencv4.1.0.sh

--- installs/opencv4.1.0.sh
+++ installs/opencv4.1.0.sh
@@ -43,10 +43,10 @@ if [ build ]; then
 fi
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE /
- -D CMAKE_INSTALL_PREFIX=/usr/local /
- -D INSTALL_PYTHON_EXAMPLES=ON /
- -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-$version/modules /
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+ -D CMAKE_INSTALL_PREFIX=/usr/local \
+ -D INSTALL_PYTHON_EXAMPLES=ON \
+ -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-$version/modules \
  -D BUILD_EXAMPLES=ON ..
 
 # Build OPENCV
