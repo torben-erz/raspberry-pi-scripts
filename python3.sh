8aec49e47f027aa36de1bcd06f73dd60d1886603
Torben Daniel Erz <3204695+torben-erz@users.noreply.github.com>
2020-07-23 13:22:28 +0200

    Update python3.sh

--- python3.sh
+++ python3.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Install python3, pip3 and numpy
+echo ""
+echo "================================"
+echo "Install python3, pip3 and numpy ..."
 sudo apt-get install python3-dev -y
 sudo apt-get install python3-pip -y
 sudo pip3 install numpy
