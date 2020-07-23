b3e6307058188cf39209b54054a0b019a0f7836e
Torben Daniel Erz <3204695+torben-erz@users.noreply.github.com>
2020-07-23 13:21:22 +0200

    Update python3.sh

--- python3.sh
+++ python3.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# System update
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade --fix-missing -y
-sudo apt autoremove
-
 # Install python3, pip3 and numpy
 sudo apt-get install python3-dev -y
 sudo apt-get install python3-pip -y
